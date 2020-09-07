0ec5f3f7c61bd7292216697ca238621b8b4c536c
bogdanmatei <matei90bogdan@yahoo.com>
2020-09-07 14:46:37 +0300

    added state to form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 //Components
 import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>My TODO List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
